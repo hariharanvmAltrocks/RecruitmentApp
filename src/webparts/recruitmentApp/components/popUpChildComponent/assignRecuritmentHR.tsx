7e3a648a65655e775ca43f7e48c0e25570db5711
karthikeyanb44 <155957401+karthikeyanb44@users.noreply.github.com>
2024-12-03 18:24:34 +0530

    assign Recuritment HR PAge Design

--- src/webparts/recruitmentApp/components/popUpChildComponent/assignRecuritmentHR.tsx
+++ src/webparts/recruitmentApp/components/popUpChildComponent/assignRecuritmentHR.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Dialog, DialogType, DialogFooter } from "@fluentui/react";
 import "../App.css";
 
 interface DynamicFormDialogProps {
@@ -12,42 +11,13 @@ interface DynamicFormDialogProps {
 }
 
 function assignRecuritmentHR(props: DynamicFormDialogProps) {
-    const footerContent = (
-        <DialogFooter>
-            <button onClick={props.onClose}>Close</button>
-        </DialogFooter>
-    );
 
-    const DialogStyles: React.CSSProperties = {
-        minWidth: props.width || "300px",
-        overflowY: "hidden",
-        zIndex: 9999,
-        borderRadius: "20px",
-        ...props.Style,
-    };
+
+
 
     return (
         <div className="Model">
-            <Dialog
-                hidden={!props.visible}
-                onDismiss={props.onClose}
-                dialogContentProps={{
-                    type: DialogType.normal,
-                    title: props.header || "",
-                    styles: DialogStyles,
-                }}
-                modalProps={{
-                    isBlocking: true,
-                    styles: {
-                        main: {
-                            borderRadius: "20px",
-                        },
-                    },
-                }}
-            >
-                {props.children}
-                {footerContent}
-            </Dialog>
+
         </div>
     );
 }
