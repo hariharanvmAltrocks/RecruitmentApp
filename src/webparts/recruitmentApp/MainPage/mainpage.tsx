67bf660938f636fb8bab3fb87d26df25a9768604
SnekaSAltrocks <155968725+SnekaSAltrocks@users.noreply.github.com>
2025-01-31 11:45:11 +0530

    ViewScorecard

--- src/webparts/recruitmentApp/MainPage/mainpage.tsx
+++ src/webparts/recruitmentApp/MainPage/mainpage.tsx
@@ -13,60 +13,126 @@ import AssignInterviewPanel from "../Screens/RecuritmentProcess/AssignInterviewP
 import InterviewPanelList from "../Screens/InterviewPanel/InterviewPanelList";
 import InterviewPanelEdit from "../Screens/InterviewPanel/InterviewPanelEdit";
 import Emptypage from "../Screens/EmptyPage/EmptyPage";
-
+import HodScoreCard from "../Screens/RecuritmentProcess/HodScoreCard";
 export default function MainPage(props: any) {
-    const { roleID, userRole, masterData, ADGroupData } = userInfo();
+  const { roleID, userRole, masterData, ADGroupData } = userInfo();
 
-    const [isExpanded, setIsExpanded] = React.useState(true);
+  const [isExpanded, setIsExpanded] = React.useState(true);
 
-    const toggleSideNav = () => {
-        setIsExpanded((prevState: any) => !prevState);
-    };
+  const toggleSideNav = () => {
+    setIsExpanded((prevState: any) => !prevState);
+  };
 
-    return (
-        <div
-            style={{ display: "flex", flexDirection: "row" }}
-            className=" ms-Grid"
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }} className=" ms-Grid">
+      <div
+        style={{ width: isExpanded ? "15%" : "6%" }}
+        onMouseEnter={() => {
+          setIsExpanded(true);
+        }}
+        onMouseLeave={() => {
+          setIsExpanded(false);
+        }}
+      >
+        <SideNavComponent roleID={roleID} IsExpanded={isExpanded} />
+      </div>
+      <div
+        style={{
+          width: isExpanded ? "85%" : "95%",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <MainPageHeader
+          toggleSideNav={toggleSideNav}
+          userName={
+            (masterData?.userDetails[0]?.FirstName ?? "") +
+            " " +
+            (masterData?.userDetails[0]?.MiddleName ?? "") +
+            " " +
+            (masterData?.userDetails[0]?.LastName ?? "")
+          }
+          userRole={userRole}
+          Department={masterData?.userDetails[0]?.DepartmentName}
         >
-
-            <div style={{ width: isExpanded ? "15%" : "6%", }}
-                onMouseEnter={() => { setIsExpanded(true) }}
-                onMouseLeave={() => { setIsExpanded(false) }}
-            >
-                <SideNavComponent roleID={roleID} IsExpanded={isExpanded} />
-            </div>
-            <div style={{ width: isExpanded ? "85%" : "95%", display: "flex", flexDirection: "column" }}>
-                <MainPageHeader toggleSideNav={toggleSideNav} userName={(masterData?.userDetails[0]?.FirstName ?? "") + " " + (masterData?.userDetails[0]?.MiddleName ?? "") + " " + (masterData?.userDetails[0]?.LastName ?? "")} userRole={userRole} Department={masterData?.userDetails[0]?.DepartmentName}>
-                    {ADGroupData?.ADGroupIDs ? (
-                        <>
-                            <Routes>
-                                <Route path="/" element={<Emptypage {...props} {...masterData} />} />
-                                <Route path="/RecurimentProcess" element={<RecruitmentProcess {...props} {...masterData} />} />
-                                <Route path="/RecurimentProcess/ApprovedVRRView" element={<ApprovedVRRView {...props} {...masterData} />} />
-                                <Route path="/RecurimentProcess/ApprovedVRREdit" element={<ApprovedVRREdit {...props} {...masterData} />} />
-                                <Route path="/CommanFieldTemplate" element={<CommanFieldTemplate {...props} {...masterData} />} />
-                                <Route path="/CommanTemplate/CommanTemplate" element={<CommanTemplate {...props} {...masterData} />} />
-                                <Route path="/RecurimentProcess/ReviewProfile" element={<ReviewProfile {...props} {...masterData} />} />
-                                <Route path="/RecurimentProcess/AssignInterviewPanel" element={<AssignInterviewPanel {...props} {...masterData} />} />
-                                <Route path="/InterviewPanelList" element={<InterviewPanelList {...props} {...masterData}  {...ADGroupData} />} />
-                                <Route path="/InterviewPanelList/InterviewPanelEdit" element={<InterviewPanelEdit {...props} {...masterData} />} />
-
-                            </Routes>
-                        </>
-                    ) : (
-                        <>
-                            <div style={{ minHeight: "100%", alignItems: "center", justifyContent: "center", display: "flex" }}>
-                                <h3 className="title" >
-                                    {"You are not assigned to any AD Group for HRMS"}
-                                </h3>
-                            </div>
-                        </>
-                    )}
-
-                </MainPageHeader>
-            </div>
-
-        </div>
-
-    );
+          {ADGroupData?.ADGroupIDs ? (
+            <>
+              <Routes>
+                <Route
+                  path="/"
+                  element={<Emptypage {...props} {...masterData} />}
+                />
+                <Route
+                  path="/RecurimentProcess"
+                  element={<RecruitmentProcess {...props} {...masterData} />}
+                />
+                <Route
+                  path="/RecurimentProcess/ApprovedVRRView"
+                  element={<ApprovedVRRView {...props} {...masterData} />}
+                />
+                <Route
+                  path="/RecurimentProcess/ApprovedVRREdit"
+                  element={<ApprovedVRREdit {...props} {...masterData} />}
+                />
+                <Route
+                  path="/CommanFieldTemplate"
+                  element={<CommanFieldTemplate {...props} {...masterData} />}
+                />
+                <Route
+                  path="/CommanTemplate/CommanTemplate"
+                  element={<CommanTemplate {...props} {...masterData} />}
+                />
+                <Route
+                  path="/RecurimentProcess/ReviewProfile"
+                  element={<ReviewProfile {...props} {...masterData} />}
+                />
+                <Route
+                  path="/RecurimentProcess/AssignInterviewPanel"
+                  element={<AssignInterviewPanel {...props} {...masterData} />}
+                />
+                <Route
+                  path="/InterviewPanelList"
+                  element={
+                    <InterviewPanelList
+                      {...props}
+                      {...masterData}
+                      {...ADGroupData}
+                    />
+                  }
+                />
+                <Route
+                  path="/InterviewPanelList/InterviewPanelEdit"
+                  element={<InterviewPanelEdit {...props} {...masterData} />}
+                />
+                <Route
+                  path="/InterviewPanelList/InterviewPanelEdit"
+                  element={<InterviewPanelEdit {...props} {...masterData} />}
+                />
+                {/* sneka */}
+                <Route
+                  path="/RecurimentProcess/HodScoreCard"
+                  element={<HodScoreCard {...props} {...masterData} />}
+                />
+              </Routes>
+            </>
+          ) : (
+            <>
+              <div
+                style={{
+                  minHeight: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  display: "flex",
+                }}
+              >
+                <h3 className="title">
+                  {"You are not assigned to any AD Group for HRMS"}
+                </h3>
+              </div>
+            </>
+          )}
+        </MainPageHeader>
+      </div>
+    </div>
+  );
 }
