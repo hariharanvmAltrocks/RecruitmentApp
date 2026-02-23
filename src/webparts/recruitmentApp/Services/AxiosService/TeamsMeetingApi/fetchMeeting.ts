60348f5bf13019ba30a8c41dc89231156c82eee2
karthikeyanb44 <155957401+karthikeyanb44@users.noreply.github.com>
2026-02-23 10:22:06 +0530

    fetch meeting api

--- src/webparts/recruitmentApp/Services/AxiosService/TeamsMeetingApi/fetchMeeting.ts
+++ src/webparts/recruitmentApp/Services/AxiosService/TeamsMeetingApi/fetchMeeting.ts
@@ -1,48 +1,49 @@
 
+import axios from "axios";
 import { IFetchMeeting } from "../axiosConfig";
 
-// const MeetingUrl = localStorage.getItem("MeetingUrl") ?? "";
-// const MeetingCode = localStorage.getItem("MeetingCode") ?? "";
+const MeetingUrl = localStorage.getItem("MeetingUrl") ?? "";
+const MeetingCode = localStorage.getItem("MeetingCode") ?? "";
 
 
-// const MeetingsAxiosInstance = axios.create({
-//     baseURL: MeetingUrl,
-//     timeout: 60000,
-//     headers: {
-//         Accept: "application/json",
-//     },
-// });
+const MeetingsAxiosInstance = axios.create({
+    baseURL: MeetingUrl,
+    timeout: 60000,
+    headers: {
+        Accept: "application/json",
+    },
+});
 
 
 
 export const fetchMeetings = async (data: IFetchMeeting): Promise<ApiResponse<string>> => {
     try {
         let AvailableMeeting = data.Email.map(async (item) => {
-            const api_url_meetings =
-                `https://dcsdocumentprocessing-a6etdefvdffedrhf.southindia-01.azurewebsites.net/api/GetMeetings` +
-                `?code=KvQiqUR0tceLcVt2iFcxqFz5Q1H19zNcHNkCVbuJTgQYAzFuEQx5vA==` +
-                `&user=${encodeURIComponent(item)}` +
-                `&start=${data.startDate}` +
-                `&end=${data.endDate}`;
+            //     const api_url_meetings =
+            //         `https://dcsdocumentprocessing-a6etdefvdffedrhf.southindia-01.azurewebsites.net/api/GetMeetings` +
+            //         `?code=KvQiqUR0tceLcVt2iFcxqFz5Q1H19zNcHNkCVbuJTgQYAzFuEQx5vA==` +
+            //         `&user=${encodeURIComponent(item)}` +
+            //         `&start=${data.startDate}` +
+            //         `&end=${data.endDate}`;
 
-            const response = await fetch(api_url_meetings, {
-                method: "GET",
-                headers: { "Accept": "application/json" }
-            });
-            // let response = await MeetingsAxiosInstance.get(
-            //     `/api/GetMeetings`,
-            //     {
-            //         params: {
-            //             code: MeetingCode,  //"G0r2jGUElBdacIOZfcoGxFid_Hu4ReaypulXOIy5oaneAzFuR_0-9g==",
-            //             user: item,
-            //             start: data.startDate,
-            //             end: data.endDate,
-            //         },
-            //         headers: {
-            //             Accept: "application/json",
-            //         },
-            //     }
-            // );
+            //     const response = await fetch(api_url_meetings, {
+            //         method: "GET",
+            //         headers: { "Accept": "application/json" }
+            //     });
+            let response = await MeetingsAxiosInstance.get(
+                `/api/GetMeetings`,
+                {
+                    params: {
+                        code: MeetingCode,  //"G0r2jGUElBdacIOZfcoGxFid_Hu4ReaypulXOIy5oaneAzFuR_0-9g==",
+                        user: item,
+                        start: data.startDate,
+                        end: data.endDate,
+                    },
+                    headers: {
+                        Accept: "application/json",
+                    },
+                }
+            );
             return response;
         })
 
--- src/webparts/recruitmentApp/Services/AxiosService/TeamsMeetingApi/scheduleMeeting.ts
+++ src/webparts/recruitmentApp/Services/AxiosService/TeamsMeetingApi/scheduleMeeting.ts
@@ -1,53 +1,52 @@
 
+import axios from "axios";
 import { ICreateMeeting } from "../axiosConfig";
 
 const MeetingUrl = localStorage.getItem("MeetingUrl") ?? "";
 const MeetingCode = localStorage.getItem("MeetingCode") ?? "";
 
 
-// const MeetingsAxiosInstance = axios.create({
-//     baseURL: MeetingUrl,
-//     timeout: 60000,
-//     headers: {
-//         Accept: "application/json",
-//     },
-// });
+const MeetingsAxiosInstance = axios.create({
+    baseURL: MeetingUrl,
+    timeout: 60000,
+    headers: {
+        Accept: "application/json",
+    },
+});
 
 
 export const createMeeting = async (data: ICreateMeeting): Promise<ApiResponse<string>> => {
     try {
 
-        const apiUrl = `${MeetingUrl}/api/CreateMeeting?code=${MeetingCode}`;
+        // const apiUrl = `${MeetingUrl}/api/CreateMeeting?code=${MeetingCode}`;
 
-        const response = await fetch(apiUrl, {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        });
+        // const response = await fetch(apiUrl, {
+        //     method: "POST",
+        //     headers: {
+        //         "Accept": "application/json",
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(data)
+        // });
 
-        // let response = await MeetingsAxiosInstance.post(
-        //     `/api/CreateMeeting?code=${MeetingCode}`,
-        //     {
-        //         params: {
-        //             code: MeetingCode,  //"G0r2jGUElBdacIOZfcoGxFid_Hu4ReaypulXOIy5oaneAzFuR_0-9g==",
-        //         },
-        //         headers: {
-        //             "Accept": "application/json",
-        //             "Content-Type": "application/json"
-        //         },
-        //         body: JSON.stringify(data),
-        //     }
-        // );
+        let response = await MeetingsAxiosInstance.post(
+            `/api/CreateMeeting?code=${MeetingCode}`,
+            {
+                params: {
+                    code: MeetingCode,  //"G0r2jGUElBdacIOZfcoGxFid_Hu4ReaypulXOIy5oaneAzFuR_0-9g==",
+                },
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
+            }
+        );
 
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
+        console.log(response, "CreateMeetingResponse");
         return {
-            data: "",
-            status: 500,
+            data: response.data,
+            status: response.status,
             message: "Meeting created successfully",
         };
     } catch (error) {
